16a778c75d744436caf356e7443537719de5d085
garicshv <garicshv@gmail.com>
2019-06-16 22:20:31 +0300

    p12

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,37 +9,5 @@ variable region {
   default = "europe-west2"
 }
 
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access"
-}
 
-variable disk_image {
-  description = "Disk image"
-}
-
-variable private_key_path {
-  description = "The path of private key location"
-}
-
-variable zone {
-	# zone
-  description = "Zone location"
-  default = "europe-west22-b"
-}
-
-variable node_count{
-	description="The count of compute instances"
-	default = "1"
-}
-
-variable app_disk_image {
-	description = "Disk image for reddit app"
-	default = "ubuntu-1604-lts"
-}
-
-variable db_disk_image {
-        description = "Disk image for reddit db"
-        default = "ubuntu-1604-lts"
-}
 
