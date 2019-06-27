968bc6cd84c1e5ba3e0acab067f6eb8774ed2b2d
garicshv <garicshv@gmail.com>
2019-06-27 22:45:48 +0300

    Created db_host environment and task for copy db connection parameters

--- ansible/ansible.scenario.sh
+++ ansible/ansible.scenario.sh
@@ -7,13 +7,25 @@ function usage(){
 	echo "$0 ping_host <appserver> - "
 	echo "$0 command <ansible-server or group> <your-command in quotas>"
 	echo "$0 store_ssh_password path-to-private-file"
+	echo "$0 check_reddit_app"
+	echo "$0 play_reddit_db"
 	exit 1
 }
 
+function check_reddit_app(){
+	ansible-playbook reddit_app.yml --check --limit app --tags app-tag
+	#--limit - ограничиваем группу хостов, для которых применить плейбук
+	#--check пробный прогон плейбука
+}
+
+function play_reddit_db(){
+	ansible-playbook reddit_app.yml --limit app --tags app-tag 
+}
 case $1 in
 	init)
 	init
 	;;
+	
 	ping_host)
 	if [ -z "$2" ]
 	then
@@ -21,6 +33,7 @@ case $1 in
 	fi
 	ping_host $2
 	;;
+	
 	command)
 	if [ -z "$2" ] || [ -z "$3" ]
         then
@@ -28,9 +41,19 @@ case $1 in
         fi
 	command $2 "$3"
 	;;
+	
 	store_ssh_password)
 	store_ssh_password ~/.ssh/appuser
 	;;
+	
+	check)
+	check_reddit_app
+	;;
+	
+	play)
+	play_reddit_db
+	;;
+
 	*)
 	usage
 	;;
